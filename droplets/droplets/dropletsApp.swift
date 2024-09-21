4a7cf444b0ba1a3319a4d2fc3be200f80076d57f
lareenmelo <lareenmmelo@gmail.com>
2024-09-21 14:55:14 -0400

    first commit 🚀! adding empty project

---
+++ droplets/droplets/dropletsApp.swift
@@ -0,0 +1,17 @@
+//
+//  dropletsApp.swift
+//  droplets
+//
+//  Created by Lareen Melo on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct dropletsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ droplets/dropletsTests/dropletsTests.swift
@@ -0,0 +1,17 @@
+//
+//  dropletsTests.swift
+//  dropletsTests
+//
+//  Created by Lareen Melo on 9/21/24.
+//
+
+import Testing
+@testable import droplets
+
+struct dropletsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
