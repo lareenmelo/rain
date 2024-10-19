a23d65241e9b56cfdb983bae7402017a2c87080f
lareenmelo <lareenmmelo@gmail.com>
2024-10-19 12:05:28 -0400

    group management

---
+++ droplets/dropletsTests/Mapping/WeatherMapperTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherMapperTests.swift
+//  dropletsTests
+//
+//  Created by Lareen Melo on 10/19/24.
+//
+
+import Testing
+
+struct WeatherMapperTests {
+
+    @Test
+	func dataMapper() {
+    }
+
+}
